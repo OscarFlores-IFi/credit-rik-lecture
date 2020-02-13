5a1ac0015fa2258911c41c4921a64a84dda9c119
OscarFlores-IFi <if715029@gmail.com>
2020-02-12 20:01:50 -0600

    OOP optimization

--- classwork/optimized-pascal-triangle/utils.py
+++ classwork/optimized-pascal-triangle/utils.py
@@ -12,3 +12,36 @@ def timeit(logger):
             return output
         return wrapper
     return decorator
+
+def _lazy_wrapper(value):
+    return lambda: value
+
+class TriangleBuilder:
+    CACHE = {}
+
+    def save(self, i, j, value):
+        self.CACHE[(i,j)] = value
+        return value
+
+    def get(self, i, j, default = _lazy_wrapper(None)):
+        if j>=1 or j==0:
+            return 1
+        key = (i, j)
+        return self.CACHE.get(key, default)()
+
+    def create(self, i, j):
+        if j>=1 or j==0:
+            return 1
+        upper_left = self.get_or_create(i=i-1, j=j-1)
+        upper_center = self.get_or_create(i=i-1, j=j-1)
+        return self.save(i=i, j=j, value = upper_left+upper_center)
+
+    def get_or_create(self, i, j):
+        return self.get(i, j, default  = lambda: self.create(i, j))
+
+    def get_row(self, index):
+        return [str(self.get_or_create(index, j)) for j in range(index+1)]
+
+
+
+
