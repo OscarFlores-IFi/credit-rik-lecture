57594b686f88cc42f76ea6819fa743cc1576e3f5
OscarFlores-IFi <if715029@gmail.com>
2020-03-05 20:08:06 -0600

    Examen1 listo

--- exam/flatten-json/main.py
+++ exam/flatten-json/main.py
@@ -1,12 +1,26 @@
-import utils
+import logging
+import fire
+import os
+
+from utils import pretty_print
+from utils import flatten_dict
 from models import load
 
+logger = logging.getLogger(__name__)
 
+class Main(object):
 
-filename = 'ex-5.json'
-tmp = load(filename)
-flatten = utils.flatten_dict(tmp)
+    @staticmethod
+    @pretty_print(logger)
+    def show(filename):
+        return load(filename)
 
+    @staticmethod
+    @pretty_print(logger)
+    def flatten(filename):
+        return flatten_dict(load(filename))
 
-
+if __name__ == "__main__":
+    logging.basicConfig(level=os.environ.get("APP_LOG_LEVEL",default="INFO"))
+    fire.Fire(Main)
 
