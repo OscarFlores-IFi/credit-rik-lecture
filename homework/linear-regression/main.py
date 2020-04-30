92b7e5ace3af200d2e38d80498745c09c37bc65e
OscarFlores-IFi <if715029@gmail.com>
2020-04-29 21:53:57 -0500

    entrega linear-regression

--- homework/linear-regression/main.py
+++ homework/linear-regression/main.py
@@ -6,19 +6,22 @@ from models import *
 
 logger = logging.getLogger(__name__)
 
+
 class Main(object):
 
     @staticmethod
-    def train_linear_regression(data_path,target_column,add_intercept='intercept', save_path='model-example.json'):
-        train(data_path,target_column,add_intercept, save_path)
+    def train_linear_regression(data_path, target_column, add_intercept='intercept', save_path='model-example.json'):
+        result = train(data_path, target_column, add_intercept, save_path)
+        return result
 
     @staticmethod
     def score_linear_regression(model_path,
-                            data_path = 'data/weather.csv',
-                            prediction = 'estimation',
-                            save_output = 'data/out.csv'):
+                                data_path='data/weather.csv',
+                                prediction='estimation',
+                                save_output='data/out.csv'):
         score(model_path, data_path, prediction, save_output)
 
+
 if __name__ == "__main__":
-    logging.basicConfig(level=os.environ.get("APP_LOG_LEVEL",default="INFO"))
+    logging.basicConfig(level=os.environ.get("APP_LOG_LEVEL", default="INFO"))
     fire.Fire(Main)
--- homework/linear-regression/models.py
+++ homework/linear-regression/models.py
@@ -1,8 +1,58 @@
-import pandas as pd
-import numpy as np
 import json
+import numpy as np
+import pandas as pd
 
-def train(data_path, target_column, add_intercept = '', save_path = ''):
+
+class LinearRegression:
+
+    def __init__(self, data=None, target_column="", add_intercept="", weights=None):
+
+        self.data = data
+        self.target_column = target_column
+        self.add_intercept = add_intercept
+        self.weights = weights
+
+    def train(self):
+
+        if self.add_intercept:
+            self.data[self.add_intercept] = np.ones(len(self.data))
+        y = self.data[self.target_column]
+        x = self.data.drop([self.target_column], axis=1)
+        w = np.linalg.pinv(((x.T).dot(x)).values).dot(x.T).dot(y)
+        return LinearRegression(self.data, self.target_column, self.add_intercept, w)
+
+    def predict(self, data_predict):
+        x = data_predict.drop([self.target_column], axis=1)
+        predict = x.dot(self.weights)
+        return predict
+
+    def save(self, name):
+        if self.add_intercept:
+            data = {
+                'target_column': self.target_column,
+                'add_intercept': self.weights[-1].tolist(),
+                'predictors': self.data.columns[:-1].tolist(),
+                'weights': self.weights[:-1].tolist()
+            }
+        else:
+            data = {
+                'target_column': self.target_column,
+                'add_intercept': 0,
+                'predictors': [i for i in self.data.columns if i != self.target_column],
+                'weights': self.weights.tolist()
+            }
+        print(data)
+        with open(name, 'w') as file:
+            json.dump(data, file)
+
+    def load(self, name):
+        with open(name, 'r') as file:
+            model = json.loads(file.read())
+
+        return model
+
+
+def train(data_path, target_column, add_intercept='', save_path=''):
     """
     Debug:
     data_path = 'data/weather.csv' or 'data/weight-height.csv'
@@ -13,33 +63,33 @@ def train(data_path, target_column, add_intercept = '', save_path = ''):
     data = pd.read_csv(data_path)
     if add_intercept:
         data[add_intercept] = np.ones(len(data))
-    y = data[target_column]
-    x = data.drop([target_column], axis=1)
-    w = np.linalg.pinv(((x.T).dot(x)).values).dot(x.T).dot(y) #calculo de pesos algebraicamente.
+    y = data[target_column].values
+    x = data.drop([target_column], axis=1).values
+    w = np.linalg.pinv(((x.T).dot(x)).values).dot(x.T).dot(y)  # calculo de pesos algebraicamente.
 
     if add_intercept:
         data = {
-            'target_column' : target_column,
-            'add_intercept' : w[-1],
-            'predictors' : [i for i in x.columns[:-1]],
-            'weights' : [i for i in w[:-1]]
-            }
+            'target_column': target_column,
+            'add_intercept': w[-1].tolist(),
+            'predictors': [i for i in x.columns if i != target_column],
+            'weights': w[:-1].tolist()
+        }
     else:
         data = {
-            'target_column' : target_column,
-            'add_intercept' : 0,
-            'predictors' : [i for i in x.columns],
-            'weights' : [i for i in w]
-            }
+            'target_column': target_column,
+            'add_intercept': 0,
+            'predictors': [i for i in x.columns if i != target_column],
+            'weights': [i for i in w]
+        }
 
     if save_path:
         with open(save_path, 'w') as file:
             json.dump(data, file)
 
-    print(data)
     return data
 
-def score(model_path, data_path = None, prediction = 'estimation', save_output = ''):
+
+def score(model_path, data_path=None, prediction='estimation', save_output=''):
     """
     Debug:
     model_path = 'model-example.json'
@@ -53,7 +103,7 @@ def score(model_path, data_path = None, prediction = 'estimation', save_output =
         model = json.loads(file.read())
 
     data = pd.read_csv(data_path)
-    x = data.drop([model['target_column']],axis=1)
+    x = data.drop([model['target_column']], axis=1)
     y_hat = x.dot(np.array(model['weights']))
 
     if prediction:
@@ -62,8 +112,4 @@ def score(model_path, data_path = None, prediction = 'estimation', save_output =
     if save_output:
         data.to_csv(save_output)
 
-    print(data)
     return data
-
-
-
